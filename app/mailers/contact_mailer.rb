fa7ec8462a2c915eac2e4d64f3850493df1da801
mochimochit <plain_4362@yahoo.co.jp>
2023-04-25 17:37:52 +0900

    add resetcss

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,5 @@
+class ContactMailer < ApplicationMailer
+  def send_mail(contact)
+    @contact = contact
+    mail to:   ENV['TOMAIL'], subject: '【お問い合わせ】' + @contact.subject_i18n
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_11_105558) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_20_102434) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
